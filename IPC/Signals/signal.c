1877e2c7c53e6666e80348368d8424823bcd68ed
vijay <vijay3770@gmail.com>
2018-06-17 13:44:08 -0400

    Updated Signal example

--- IPC/Signals/signal.c
+++ IPC/Signals/signal.c
@@ -2,17 +2,26 @@
 #include <unistd.h>
 #include <signal.h>
 
-void sigal_handle(int signum)
+void signal_handle(int signum)
 {
-    printf("Inside the signal handle of %d", signum);
+    printf("Inside the signal handle of %d\n", signum);
 }
+
 
 int main()
 {
+	void (*sigHandlerReturn)(int);
     printf("Open new terminal and issue the following command\n");
-    printf("kill -SIGINT %d\n", getpid());
-    
-    signal(SIGINT, sigal_handle);
+    printf("kill -SIGINT %d or hit ctl+c on the process running terminal\n", getpid());
+
+	sigHandlerReturn = signal(SIGINT, signal_handle);
+
+	if(sigHandlerReturn < 0)
+	{
+		perror("signal Error");
+		return -1;
+	}
+
     while(1)
     {
         
